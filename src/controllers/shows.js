0c07e8c961cd797dd773c5a4dabfbfe22302b85e
Amir Dar <dardar4@gmail.com>
2021-01-10 11:49:00 +0200

    added subscription dal

--- src/controllers/shows.js
+++ src/controllers/shows.js
@@ -1,27 +1,28 @@
 const SubscriptionAPI = require('../External API/SubscriptionAPI');
+const subscriptionsDal = require('../dal/subscriptions');
 
 
 const getAllShows = async () => {
     // Get all shows
-    const result = await SubscriptionAPI.getShows();
+    const result = await subscriptionsDal.getShows();
     return result.data;
 };
 
 const addShow = async (showData) => {
     // Create new show
-    const result = await SubscriptionAPI.addShow(showData);
+    const result = await subscriptionsDal.addShow(showData);
     return result.data;
 }
 
 const updateShow = async (showID, showData) => {
     // Update existing show
-    const result = await SubscriptionAPI.updateShow(showID, showData);
+    const result = await subscriptionsDal.updateShow(showID, showData);
     return result.data;
 }
 
 const deleteShow = async (showID) => {
     // Delete existing show
-    const result = await SubscriptionAPI.deleteShow(showID);
+    const result = await subscriptionsDal.deleteShow(showID);
     return result.data;
 }
 
